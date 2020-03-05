328491591e52e20e220e5fc6bb99bd2c71c58e8f
bgpark <bgpark82@gmail.com>
2020-03-06 01:45:00 +0900

    Remove cypress directory

--- cypress/integration/sample_spec.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test',function(){
-    it('Does not do much!',function(){
-        expect(true).to.equal(true);
-    })
-    // it('Visits the Letshadow',function(){
-    //     cy.visit('http://letshadow.com')
-    // })
-})
-
