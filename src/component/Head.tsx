071ed9818a7b8ba44ef6675ed80148e3f5220d33
bgpark <bgpark82@gmail.com>
2020-03-07 13:33:45 +0900

    Add Hamberger icon to toggle the navbar

--- src/component/Head.tsx
+++ src/component/Head.tsx
@@ -1,26 +1,55 @@
+import { css } from '@emotion/core';
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const HeaderBlock = styled.div`
+const HeaderTemplate = styled.div`
+  font-size: 1.125rem;
+  font-weight: 800;
   display: flex;
-  padding: 1.2rem 25rem 1rem 25rem;
-
+  justify-content: space-around;
+  align-items: center;
   border-bottom: 1px solid #e9ecef;
+  min-height: 3.6rem;
+`;
+
+const Logo = styled.div``;
+
+const Profile = styled.div``;
+
+const HeaderBlock = styled.div<{ toggle: boolean }>`
+  display: flex;
+  padding: 1rem;
   @media screen and (max-width: 768px) {
-    width: 100%;
-    overflow-x: auto;
+    position: absolute;
+    top: 3.6rem;
+    background: white;
+    height: 100%;
+    left: 0;
+    width: 50%;
+    flex-direction: column;
+    transform: translateX(-100%);
+    opacity: 0;
+
+    ${props =>
+      props.toggle
+        ? css`
+            transform: translateX(0%);
+            opacity: 100%;
+            transition: transform 0.3s, opacity 0.3s;
+          `
+        : css`
+            transition: transform 0.8s, opacity 0.3s;
+          `}
   }
 `;
 
 const Header = styled(NavLink)`
-  font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
   text-decoration: none;
   color: inherit;
   padding-bottom: 0.25rem;
-  font-weight: 800;
 
   margin-right: 1rem;
   &:hover {
@@ -28,11 +57,25 @@ const Header = styled(NavLink)`
   }
 `;
 
+const Burger = styled.div`
+  display: none;
+  cursor: pointer;
+  & div {
+    width: 20px;
+    height: 3px;
+    background-color: #7950f2;
+    margin: 4px;
+  }
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
 const activeStyle = {
   color: '#7950f2',
 };
 
-function Head({ onSelect, header }: any) {
+function Head() {
   const headerList = [
     {
       name: 'all',
@@ -64,20 +107,34 @@ function Head({ onSelect, header }: any) {
     },
   ];
 
+  const [toggle, setToggle] = useState(false);
+  const onToggle = () => {
+    setToggle(!toggle);
+    console.log(toggle);
+  };
+
   return (
-    <HeaderBlock>
-      {headerList.map(h => (
-        <Header
-          key={h.name}
-          activeStyle={activeStyle}
-          onClick={() => onSelect(h.name)}
-          to={h.name === 'all' ? '/' : `/${h.name}`}
-          exact
-        >
-          {h.text}
-        </Header>
-      ))}
-    </HeaderBlock>
+    <HeaderTemplate>
+      <Burger onClick={onToggle}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Burger>
+      <Logo>Letshadow</Logo>
+      <HeaderBlock toggle={toggle}>
+        {headerList.map(h => (
+          <Header
+            key={h.name}
+            activeStyle={activeStyle}
+            to={h.name === 'all' ? '/' : `/${h.name}`}
+            exact
+          >
+            {h.text}
+          </Header>
+        ))}
+      </HeaderBlock>
+      <Profile>프로필</Profile>
+    </HeaderTemplate>
   );
 }
 
